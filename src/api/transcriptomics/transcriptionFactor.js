d00b46287b47bd00e7882707395a285bca0c3b96
x8188 <1341002157@qq.com>
2023-03-23 20:50:32 +0800

    tfbd接口

--- src/api/transcriptomics/transcriptionFactor.js
+++ src/api/transcriptomics/transcriptionFactor.js
@@ -1,6 +1,45 @@
-import request from '@/utils/request'
+import request from "@/utils/request";
 
-export const reqMultiTissue = () =>
-  request({ url: `/zeamap/tissue/multi`, method: "get" });
+export const reqSelectReference = () =>
+  request({ url: `/tran/fator/info/selectReference`, method: "get" });
 
-  
+export const reqSelectVersion = (accession) =>
+  request({
+    url: `/tran/fator/info/selectVersion`,
+    method: "get",
+    // data: {"accession":accession},
+    params: { accession: accession },
+    // data: accession,
+    // headers: {
+    //   "Content-Type": "multipart/form-data",
+    // },
+  });
+
+export const reqSelectAnalysis = () =>
+  request({ url: `/tran/fator/info/selectAnalysis`, method: "get" });
+
+export const reqSelectTFID = () =>
+  request({ url: `/tran/fator/info/selectTFID`, method: "get" });
+
+export const reqSelectTFName = () =>
+  request({ url: `/tran/fator/info/selectTFName`, method: "get" });
+
+export const reqSelectTFFamily = () =>
+  request({ url: `/tran/fator/info/selectTFFamily`, method: "get" });
+
+export const reqSelectTFGeneID = () =>
+  request({ url: `/tran/fator/info/selectTFGeneID`, method: "get" });
+
+export const reqSelectInfo = (data) =>
+  request({
+    url: `/tran/fator/info/selectInfo`,
+    method: "post",
+    data: data,
+    headers: {
+      // "Content-Type": "multipart/form-data; boundary=--------------------------738612952322689331490464",
+      "Content-Type": "multipart/form-data;",
+    },
+  });
+
+export const reqDownload = (data) =>
+  request({ url: `/tran/fator/info/download`, method: "get", data: data });
