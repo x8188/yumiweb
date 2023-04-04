39d60bf2cd0fc513e37ad5613b87d9fdbfe377c8
Sayakure <tosw18698204833@163.com>
2023-04-04 10:19:16 +0800

    .

---
+++ src/views/analysis/components/sidebar.vue
@@ -0,0 +1,163 @@
+<template>
+    <div class="menu-container" :class="{ hide }" >
+      <div class="header">
+        <i v-show="!hide"  class="el-icon-s-fold" style="font-size: 30px;color: #489E38;" @click="changeShow"></i>
+        <i v-show="hide"  class="el-icon-s-unfold" style="font-size: 30px;color: #489E38;"  @click="changeShow"></i>
+      </div>
+        <div class="menu-list">
+          <div class="menu-item">
+            <div style="width: 90%;">
+              <el-input
+                style="width: 90%;margin-top: 10px;"
+                placeholder="Descirption"
+                v-model="descirption">
+                <i slot="prefix" class="el-input__icon el-icon-search"></i>
+              </el-input>
+            </div>
+          </div>
+          <div v-for="(name,index) in filterNames" class="menu-item">
+            <div :index="String(index)"  style="width: 90%;">
+              <!-- bfc -->
+              <span class="title" >{{ name }}</span>
+              <el-select index="0" v-model="filters[name]" placeholder="" style="width: 90%;margin-top: 10px;">
+                <el-option
+                v-for="(item,i) in options[name]"
+                :key="i"
+                :label="item"
+                :value="item"
+                ></el-option>
+              </el-select>
+          </div>
+          </div>
+        </div>
+        <div  class="footer">
+          <el-button size="small" @click="clearPhenomics" style="margin-right: 15px;">
+            <SvgIcon icon-class="CLEAR" color="20AE35" style="margin-right: 7px;margin-left: 0;"></SvgIcon>
+            <span style="color: #20AE35">清空</span>
+          </el-button>
+          <el-button type="primary" size="small" @click="checkPhenomics">
+            查询
+              <SvgIcon icon-class="search" color="fff" style="margin-left: 7px;"></SvgIcon>
+          </el-button>
+        </div>
+    </div>
+</template>
+
+<script>
+import SvgIcon from '@/components/CommonComponents/SvgIcon.vue'
+export default {
+components: { SvgIcon },
+data() {
+return {
+    descirption: '',
+    hide: false,
+    filterNames: ['Omics', 'Analysis Id'],
+  filters: {
+    Category: '',
+    Type: '',
+    Analysis: '',
+    Name: '',
+    Location: '',
+    TraitDateLoc: '',
+    Year: ''
+  },
+  options: {
+    Category: '',
+    Type: '',
+    Analysis: '',
+    Name: '',
+    Location: '',
+    TraitDateLoc: '',
+    Year: ''
+  }
+  }
+},
+created() {
+this.getPhenomicsDropDown()
+},
+methods: {
+changeShow() {
+  this.hide = !this.hide
+},
+// 获取下拉框数据
+async getPhenomicsDropDown() {
+  // const { data }= await getPhenomicsDropDown()
+  // // 这块用解构赋值怎么写来着 忘了 回来改
+  // this.options.Category = data.Category
+  // this.options.Type = data.Type
+  // this.options.Analysis = data.Analysis
+  // this.options.Name = data.Name.slice(1)
+  // this.options.Location = data.Location
+  // this.options.TraitDateLoc = data.TraitDateLoc
+  // this.options.Year = data.Year
+},
+// 传信息
+checkPhenomics() {
+  this.$emit('getFilterData', this.filters)
+},
+// 清空数据
+clearPhenomics() {
+  this.filters = {
+    Category: '',
+    Type: '',
+    Analysis: '',
+    Name: '',
+    Location: '',
+    TraitDateLoc: '',
+    Year: ''
+  }
+  this.$emit('getFilterData', this.filters)
+},
+}
+}
+</script>
+
+<style lang="scss" scoped>
+.menu-container {
+  width: 20%;
+  max-width:300px;
+  height: 400px;
+  border-right: 1px solid #DCDFE6;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  padding-top: 25px;
+}
+
+.menu-list {
+  flex: 1;
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+  .menu-item {
+    display: flex;
+    justify-content: flex-end;
+  }
+}
+.footer {
+  margin-top: 20px;
+  margin-right: 20px;
+  display: flex;
+  justify-content: flex-end
+}
+.header {
+  margin-bottom: 20px;
+  margin-left: 20px;
+}
+.title {
+  font-weight: 700;
+  color: #727377;
+  font-size: 14px;
+}
+.hide {
+  width: 30px;
+  padding-right: 20px; 
+  margin-right: 20px;
+  border: none;
+  .menu-list,
+  .footer {
+    display: none;
+  }
+  
+}
+</style>
---
+++ src/views/analysis/index.vue
@@ -0,0 +1,151 @@
+<template>
+    <div class="phenomics">
+        <transition name="fade-transform" mode="out-in">
+          <div class="phenomics-container" v-show="!showInfo">
+            <SideBar @getFilterData="getFilterData" />
+            <div class="data-container">
+              <div class="data-top">
+                <div class="info-nums">
+                  <span>Show</span>
+                  <el-select @change="changeResultsNums" v-model="page.pageSize" style="width: 80px;margin: 0 10px;">
+                    <el-option label="10" :value="10"></el-option>
+                    <el-option label="15" :value="15"></el-option>
+                    <el-option label="20" :value="20"></el-option>
+                    <el-option label="25" :value="25"></el-option>
+                    <el-option label="50" :value="50"></el-option>
+                  </el-select>
+                  <span>results</span>
+                </div>
+              </div>
+              <div class="data-table" style="margin-top: 30px;">
+                <el-table
+                v-loading="loading"
+                ref="multipleTable"
+                :data="tableData"
+                tooltip-effect="dark"
+                style="width: 100%"
+                >
+                <el-table-column
+                  label="Omics"
+                  width="250px"
+                >
+                  <template slot-scope="scope" >
+                    <div class="" @click="changeShowInfo(scope)">
+                      <span class="origin-link-green" >
+                      {{ scope.row.xotUid }}
+                      </span>
+                    </div>
+                  </template>
+                </el-table-column>
+                <el-table-column
+                  label="Analysis Id"
+                  prop="AnalysisId"
+                >
+                </el-table-column>
+                <el-table-column
+                  label="Show description"
+                  prop="Show description"
+                  show-overflow-tooltip>
+                </el-table-column>
+              </el-table>
+              <el-pagination
+                background
+                :page-size="page.pageSize"
+                @current-change="changePage"
+                layout="prev, pager, next"
+                :total="page.total"
+                style="margin-top: 25px;margin-bottom: 50px;float: right;">
+              </el-pagination>
+              </div>
+            </div>
+          </div>
+        </transition>
+      
+      <transition name="fade-transform" mode="out-in">
+        <AnalysisInfo :show-info.sync="showInfo" :info-item="infoItem"/>
+      </transition>
+    </div>
+</template>
+
+<script>
+import AnalysisInfo from './components/analysis-info.vue'
+import SvgIcon from '@/components/CommonComponents/SvgIcon.vue'
+import SideBar from './components/sidebar.vue'
+export default {
+components: { SideBar, SvgIcon, AnalysisInfo },
+data() {
+  return {
+    page: {
+      pageNum : 1,
+      pageSize: 10,
+      total: 0
+    },
+    loading: false,
+    tableData: [],
+    multipleSelection: [],
+    showInfo: false,
+    infoItem: {}
+  }
+},
+created() {
+  this.getPhenomics()
+},
+methods: { 
+  // 获取phenomics的信息
+  async getPhenomics() {
+    this.loading = true
+    
+    this.loading = false
+
+  },
+    // 获取筛选数据
+    getFilterData(filter) {
+    this.page = {...this.page, ...filter}
+    this.getPhenomics()
+  },
+  // 改变每页展示的信息条数
+  changeResultsNums(newVal) {
+    this.page.pageSize = newVal
+  },
+  // 修改所在的页数
+  changePage(newVal) {
+    this.page.pageNum = newVal
+  },
+  // 展示详情信息
+  changeShowInfo({ row }) {
+    this.showInfo = true
+    this.infoItem = row
+  }
+},
+computed: {
+}
+}
+</script>
+
+<style lang="scss" scoped>
+.phenomics {
+  width: 100%;
+}
+.phenomics-container {
+display: flex;
+justify-content: center;
+}
+::v-deep .el-submenu__title:hover {
+background-color: #fff;
+}
+::v-deep .el-submenu__icon-arrow {
+visibility: hidden
+}
+.data-container {
+min-width: 1200px;
+width: 100%;
+padding: 25px 15px;
+}
+.data-top {
+display: flex;
+justify-content: space-between;
+}
+.phenomics-container {
+  transition: all .5s;
+}
+</style>
