64792ef474c7b071794dc648e58f19f51a19895e
x8188 <1341002157@qq.com>
2023-04-12 18:05:26 +0800

    qtl

--- src/views/genetics/sreachQTL/components/geno-viewer.vue
+++ src/views/genetics/sreachQTL/components/geno-viewer.vue
@@ -179,9 +179,9 @@
 </template>
 <script>
 import SvgIcon from "@/components/CommonComponents/SvgIcon.vue";
-import Title from "@/components/CommonComponents/Title.vue";
+// import Title from "@/components/CommonComponents/Title.vue";
 export default {
-  components: { Title, SvgIcon },
+  components: { SvgIcon },
   data() {
     return {
       qtlType: "association",
@@ -210,12 +210,11 @@ export default {
         LinkMap: [],
       },
       exportLoading: false,
-      tableShow:false
+      tableShow: false,
     };
   },
   created() {
     this.getdata();
-    // this.sqldownload();
   },
   methods: {
     async getdata() {
@@ -337,10 +336,26 @@ export default {
         let res = await this.$API.Qtl.reqassociation_qtl(data);
 
         if (res.code == 200) {
-          console.log(res);
-          this.tableShow=true
+          this.$emit("showResult", res.data,data);
         }
       } else {
+        let data = {
+          accession: "B73",
+          version: "4.43.0",
+          omics: "Phenomics",
+          xot_uid: "Agro2-Kernel_Weight_BLUP",
+          linkagemap: "",
+          chr: "",
+          start: 1,
+          end: 999999999,
+          lod_min: 0.01,
+          lod_max: 1000.88,
+        };
+        let res = await this.$API.Qtl.reqlinkage(data);
+
+        if (res.code == 200) {
+          this.$emit("showResult", res.data,data);
+        }
       }
     },
     changeType() {
@@ -358,36 +373,6 @@ export default {
         QTLend: "",
         LinkMap: "",
       };
-    },
-
-    sqldownload() {
-      this.$confirm("是否确认导出出qtl数据项?", "警告", {
-        confirmButtonText: "确定",
-        cancelButtonText: "取消",
-        type: "warning",
-      })
-        .then(() => {
-          this.exportLoading = true;
-          let data = {
-            accession: "B73",
-            version: "4.43.0",
-            omics: "Phenomics",
-            xot_uid: "Agro2-Row_Kernel_Number_BLUP",
-            chr: "",
-            start: 100000000,
-            end: 200000000,
-            log_min: 0.01,
-            log_max: 999999999,
-          };
-          this.download(
-            "genetics/search_qtl/association_qtl/download",
-            {
-              ...data,
-            },
-            `association_qtl_${new Date().getTime()}.xlsx`
-          );
-        })
-        .catch(() => {});
     },
   },
 };
