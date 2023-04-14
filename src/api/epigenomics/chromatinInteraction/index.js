ea2911dce711d4637cab4b6b636358e8b66f69e5
Sayakure <tosw18698204833@163.com>
2023-04-14 11:22:23 +0800

    .

---
+++ src/api/epigenomics/chromatinInteraction/index.js
@@ -0,0 +1,53 @@
+import request from '@/utils/request'
+
+// 下拉框选analysis
+export function dropDownAnalysis() {
+  return request({
+    url: '/chromatin_interaction/selectanalysis?accession=B73&version=4.43.0'
+  })
+}
+
+// 下拉框选择version
+export function dropDownVersion() {
+  return request({
+    url: '/chromatin_interaction/selectversion?accession=B73',
+  })
+}
+
+// 下拉框选择accession
+export function dropDownAccession() {
+  return request({
+    url: '/chromatin_interaction/selectaccession',
+  })
+}
+
+// 查找chrA
+export function queryChrA() {
+  return request({
+    url: '/chromatin_interaction/selectchrA'
+  })
+}
+
+// 查找chrB
+export function queryChrB() {
+  return request({
+    url: '/chromatin_interaction/selectchrB'
+  })
+}
+
+// 大查询
+export function queryAll(params) {
+  return request({
+    url: '/chromatin_interaction/selectchromatin_interaction',
+    params
+  })
+}
+
+// 下载
+export function downloadAll(data) {
+  return request({
+    url: '/chromatin_interaction/download',
+    method: 'post',
+    data
+  })
+}
--- src/main.js
+++ src/main.js
@@ -38,6 +38,9 @@ import VueMeta from 'vue-meta'
 // 字典数据组件
 import DictData from '@/components/DictData'
 
+// 引入百度地图
+import BaiduMap from 'vue-baidu-map'
+
 // 自己的UI组件
 import UI from '@/components/UnionStyle/index'
 // 定义API接口
@@ -65,6 +68,9 @@ Vue.component('ImageUpload', ImageUpload)
 Vue.component('ImagePreview', ImagePreview)
 Vue.component('UI',UI)
 
+Vue.use(BaiduMap, {
+  ak: 'YOUR_APP_KEY'    //这个地方是官方提供的ak密钥
+})
 Vue.use(directive)
 Vue.use(plugins)
 Vue.use(VueMeta)
