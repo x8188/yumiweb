3a0530f7cc5614167457c07a854e074253f33fd0
x8188 <1341002157@qq.com>
2023-04-26 16:54:18 +0800

    add GwasSingleVariantPage

---
+++ src/views/gwasTool/GwasSingleVariant/desktop/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <q-page-container id="GwasSingleVariantPage">
+    <q-page class="q-mt-lg" style="margin-bottom: 100px;">
+      <UILeftBorderedTitleCard :title="$t('gwasSingleVariant')">
+        <p>{{ $t('message') }}</p>
+        <div class="row select-line-height search-filter">
+          <p class="q-mr-sm q-mb-sm">{{ $t('search') }}</p>
+          <q-select
+            v-model="variant"
+            bg-color="white"
+            borderless
+            class="selector"
+            dense
+            fill-input
+            hide-dropdown-icon
+            hide-selected
+            maxlength="64"
+            :options="variantOpts"
+            options-dense
+            outlined
+            style="width: 320px;"
+            use-input
+            @filter="variantFilterFn"
+            @input="selectVariant"
+          >
+            <template #no-option>
+              <q-item>
+                <q-item-section class="text-grey">{{ $t('noResults') }}</q-item-section>
+              </q-item>
+            </template>
+          </q-select>
+        </div>
+      </UILeftBorderedTitleCard>
+      <UILeftBorderedTitleCard class="q-mt-md" :title="$t('result')">
+        <div v-if="isIE" class="text-negative">{{ $t('toGoogle') }}</div>
+        <div v-if="isNoResult" class="text-bold">{{ $t('noPhewas') }}</div>
+        <div id="plot" />
+      </UILeftBorderedTitleCard>
+    </q-page>
+  </q-page-container>
+</template>
+
+<script src="./script.js"></script>
+
+<i18n src="../common/i18n.json"></i18n>
+
+<style lang="scss" src="./style.scss"></style>
---
+++ src/views/gwasTool/GwasSingleVariant/mobile/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="MGwasSingleVariantPage">
+    <q-expansion-item
+      class="m-filter-panel sticky-position bg-primary text-white shadow-2"
+      expand-icon-class="text-white"
+      icon="fas fa-filter"
+      :label="$t('filters')"
+      style="z-index: 3; top: 50px;"
+    >
+      <q-card class="bg-primary">
+        <q-card-section>
+          <div class="row q-col-gutter-md">
+            <div class="col-12">
+              <p class="q-mb-sm">{{ $t('search') }}</p>
+              <q-select
+                v-model="variant"
+                bg-color="white"
+                borderless
+                class="selector"
+                dense
+                fill-input
+                hide-dropdown-icon
+                hide-selected
+                maxlength="64"
+                :options="variantOpts"
+                options-dense
+                outlined
+                use-input
+                @filter="variantFilterFn"
+                @input="selectVariant"
+              >
+                <template #no-option>
+                  <q-item>
+                    <q-item-section class="text-grey">{{ $t('noResults') }}</q-item-section>
+                  </q-item>
+                </template>
+              </q-select>
+            </div>
+          </div>
+        </q-card-section>
+      </q-card>
+    </q-expansion-item>
+    <div v-if="isNoResult" class="text-bold q-ma-md">{{ $t('noPhewas') }}</div>
+    <div id="plot" />
+  </div>
+</template>
+
+<script src="./script.js"></script>
+
+<i18n src="../common/i18n.json"></i18n>
