81915c276fb215309ef83be9bee0a8f2dcb3123a
Sayakure <tosw18698204833@163.com>
2023-05-20 14:12:29 +0800

    ldviewer暂存

--- src/views/variation/LDViewer/components/bbmap.vue
+++ src/views/variation/LDViewer/components/bbmap.vue
@@ -1,155 +1,233 @@
 <template>
  <div class="">
     <div id="ldCanvas" style="position: relative; top: 50px; left: 80px;height: 900px;" @mousemove="mouseInit">
-        <canvas id="geneNames" width="620" height="500" style="position: absolute; top: 0px; left: 0px;"></canvas>
-        <canvas class="geneCanvas" id="geneCanvas" width="620" height="600" style="position: absolute; top: 420px; left: 0px;"></canvas>
+        <canvas ref='nameCanvas' @mousemove="mousemoveNames" id="geneNames" width="620" height="500" style="position: absolute; top: 0px; left: 0px;"></canvas>
+        <canvas ref="geneCanvas" class="geneCanvas" id="geneCanvas" width="620" height="600" style="position: absolute; top: 420px; left: 0px;" @mousemove="mousemoveCanvas"></canvas>
         <div class="color-side-bar" style="position: absolute;left: 0;top: 50%;">
             <img src="@/assets/images/color-sidebar.png" alt="">
         </div>
-        <div class="gene-info" style="position: absolute;" :style="{left: screen.screenX+'px',top: screen.screenY+'px'}" v-show="showInfo">
-            4523458234532
+        <div class="gene-info" style="position: absolute;" :style="{left: screen.screenX+150+'px',top: screen.screenY+'px'}" v-show="showInfo">
+            {{ diamondInfo }}
         </div>
     </div>
  </div>
 </template>
 
 <script>
-
 export default {
-    data() {
-        return {
-            screen: {
-                screenX: 0,
-                screenY : 0
-            }
+  data() {
+    return {
+      screen: {
+        screenX: 0,
+        screenY: 0,
+      },
+      diamonds: [],
+      diamondInfo: "",
+      firstLineNum: 42,
+      lines: [],
+      highlightedLine: null,
+    };
+  },
+  computed: {
+    showInfo() {
+      return this.screen.screenY > 360 && this.screen.screenX < 600;
+    },
+  },
+  mounted() {
+    this.initCanvas();
+    this.initNames();
+  },
+  methods: {
+    initColor() {
+      const deep1 = "#000";
+      const deep2 = "#262626";
+      const deep3 = "#565656";
+      const deep4 = "#CECECE";
+      const deep5 = "#FFFFFF";
+      let firstLineNum = this.firstLineNum;
+      let colorArr = new Array();
+      for (let i = 0; i < firstLineNum; i++) {
+        colorArr[i] = new Array(i);
+        for (let j = 0; j < firstLineNum - i; j++) {
+          colorArr[i][j] = "#A5A5A5";
         }
+      }
+      return colorArr;
     },
-    computed: {
-        showInfo() {
-            return this.screen.screenY > 450 && this.screen.screenX < 600
+    initCanvas() {
+      let canvas = document.querySelector("#geneCanvas");
+      let ctx = canvas.getContext("2d");
+      ctx.lineWidth = 2;
+      let beginX = 10;
+      let beginY = 10;
+      let halfWidth = 6;
+      let firstLineNum = this.firstLineNum;
+      const colorArr = this.initColor();
+      for (let i = 0; i < firstLineNum; i++) {
+        // 21列
+        for (let j = 0; j < firstLineNum - i; j++) {
+          // 每行的格子数
+          ctx.strokeStyle = "#fff";
+          ctx.fillStyle = colorArr[i][j];
+          ctx.beginPath();
+          ctx.moveTo(beginX, beginY);
+          beginX += halfWidth;
+          beginY -= halfWidth;
+          ctx.lineTo(beginX, beginY);
+          beginX += halfWidth;
+          beginY += halfWidth;
+          ctx.lineTo(beginX, beginY);
+          beginX -= halfWidth;
+          beginY += halfWidth;
+          ctx.lineTo(beginX, beginY);
+          ctx.closePath();
+          ctx.stroke();
+          this.diamonds.push({
+            x: beginX - halfWidth,
+            y: beginY - halfWidth,
+            width: halfWidth * 2,
+            height: halfWidth * 2,
+            color: colorArr[i][j],
+            nameX: i,
+            nameY: j,
+            borderColor: "#fff",
+          });
+
+          ctx.fill();
+          // 初始化 beginX beginY
+          beginX += halfWidth + 2.2;
+          beginY -= halfWidth;
         }
+        beginX = 10 + halfWidth * (i + 1) + 1 * i;
+        beginY = 10 + halfWidth * (i + 1) + 1 * i;
+      }
+      ctx.fill();
     },
-    mounted() {
-        this.initCanvas()
-        this.initNames()
+    mouseInit(e) {
+      this.screen.screenX = e.clientX - 360;
+      this.screen.screenY = e.clientY + window.pageYOffset - 1030;
     },
-    methods: {
-        initColor() {
-            const deep1 = '#000'
-            const deep2 = '#262626'
-            const deep3 = '#565656'
-            const deep4 = '#CECECE'
-            const deep5 = '#FFFFFF'
-            let firstLineNum = 42
-            let colorArr = new Array()
-            for(let i=0;i< firstLineNum; i++) {
-                colorArr[i] = new Array(i)
-                for(let j=0;j < firstLineNum-i;j++) {
-                    colorArr[i][j] = '#A5A5A5'
-                }
-            }
-            return colorArr
-        },
-        initCanvas() {
-            let canvas = document.querySelector('#geneCanvas')
-            let ctx = canvas.getContext('2d')
-            ctx.lineWidth = 2
-            let beginX = 10
-            let beginY = 10
-            let halfWidth = 6
-            let firstLineNum = 42
-            const colorArr = this.initColor()
-            for(let i= 0; i < firstLineNum; i++) {
-                // 21列
-                for(let j=0; j < firstLineNum -i;j++) {
-                    // 每行的格子数
-                    ctx.strokeStyle = '#fff'
-                    ctx.fillStyle = colorArr[i][j]
-                    ctx.beginPath()
-                    ctx.moveTo(beginX, beginY)
-                    beginX += halfWidth
-                    beginY -= halfWidth
-                    ctx.lineTo(beginX, beginY)
-                    beginX += halfWidth
-                    beginY += halfWidth
-                    ctx.lineTo(beginX, beginY)
-                    beginX -= halfWidth
-                    beginY += halfWidth
-                    ctx.lineTo(beginX, beginY)
-                    ctx.closePath()
-                    ctx.stroke()
-                    ctx.fill()
-                    // 初始化 beginX beginY
-                    beginX += halfWidth + 2.2
-                    beginY -= halfWidth
-                }
-                beginX = 10 + halfWidth*(i+1) + 1*i 
-                beginY = 10 + halfWidth*(i+1)  + 1*i
-            }
-            ctx.fill()
-        },
-        initNames(){
-            let canvas = document.querySelector('#geneNames')
-            let ctx = canvas.getContext('2d')
-            ctx.lineWidth = 2
-            let firstLineNum = 42
+    mousemoveCanvas(event) {
+      let rect = this.$refs.geneCanvas.getBoundingClientRect();
+      let x = event.clientX - rect.left;
+      let y = event.clientY - rect.top;
 
-            let nameStart = 15
-            let nameWidth = 14.3
+      for (let i = 0; i < this.diamonds.length; i++) {
+        let diamond = this.diamonds[i];
+        if (
+          x < diamond.x &&
+          x > diamond.x - diamond.width &&
+          y < diamond.y &&
+          y > diamond.y - diamond.height
+        ) {
+          this.diamondInfo = `x: ${diamond.nameX}   y: ${diamond.nameY}`;
+          break;
+        }
+      }
+    },
 
-            for(let i= 0; i < firstLineNum; i++) {
-                // 每行的格子数
-                ctx.strokeStyle = '#999'
-                ctx.beginPath()
-                ctx.moveTo(nameStart+nameWidth*i, 180)
-                ctx.lineTo(nameStart+nameWidth*i, 400)
-                ctx.closePath()
-                ctx.stroke()
+    initNames() {
+      let canvas = document.querySelector("#geneNames");
+      let ctx = canvas.getContext("2d");
+      ctx.lineWidth = 2;
+      let firstLineNum = this.firstLineNum;
 
-                // 添加旋转后的文字
-                ctx.save(); // 保存当前绘图环境的状态
-                ctx.translate(nameStart+nameWidth*i, 0); // 平移绘图环境，将旋转的中心点设置为文字的起始位置
-                ctx.transform(0, 1, -1, 0, 0, 0); // 旋转-90度，字形变换
-                ctx.font = '12px Arial';
-                ctx.fillText('Hello', 0, 0); // 旋转后的位置为 (0, 0)
-                ctx.restore(); // 恢复之前保存的绘图环境的状态
+      let nameStart = 15;
+      let nameWidth = 14.3;
 
-                ctx.fill()
-            }
-        },
-        mouseInit(e) {
-           this.screen.screenX = e.clientX - 360
-           this.screen.screenY = e.clientY + window.pageYOffset - 1030
-           console.log(this.screen);
-        },
-        
-    }
-}
+      for (let i = 0; i < firstLineNum; i++) {
+        this.lines.push({
+          x: nameStart + nameWidth * i,
+          y: 180,
+          width: 2,
+          height: 220,
+          column: i,
+        });
+      }
+
+      this.drawLines(ctx);
+    },
+    mousemoveNames(event) {
+      let rect = this.$refs.nameCanvas.getBoundingClientRect();
+      let x = event.clientX - rect.left;
+      let y = event.clientY - rect.top;
+
+      let lineFound = null;
+
+      for (let i = 0; i < this.lines.length; i++) {
+        let line = this.lines[i];
+        if (
+          x > line.x - 5 &&
+          x < line.x + line.width + 5 &&
+          y > line.y &&
+          y < line.y + line.height
+        ) {
+          lineFound = line;
+          break;
+        }
+      }
+
+      this.highlightedLine = lineFound;
+
+      let ctx = this.$refs.nameCanvas.getContext("2d");
+      ctx.clearRect(
+        0,
+        0,
+        this.$refs.nameCanvas.width,
+        this.$refs.nameCanvas.height
+      );
+      this.drawLines(ctx);
+    },
+    drawLines(ctx) {
+      for (let i = 0; i < this.lines.length; i++) {
+        let line = this.lines[i];
+
+        ctx.strokeStyle = line === this.highlightedLine ? "red" : "#999";
+
+        ctx.beginPath();
+        ctx.moveTo(line.x, line.y);
+        ctx.lineTo(line.x, line.y + line.height);
+        ctx.closePath();
+        ctx.stroke();
+
+        ctx.save();
+        ctx.translate(line.x, 0);
+        ctx.transform(0, 1, -1, 0, 0, 0);
+
+        ctx.fillStyle = line === this.highlightedLine ? "red" : "#000";
+        ctx.font = "12px Arial";
+        ctx.fillText("Hello", 0, 0);
+        ctx.restore();
+        ctx.fill();
+      }
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
 .color-side-bar {
-    img {
-        position: absolute;
-        left: -28px;
-        top: 108px;
-        width: 80px;
-    }
+  img {
+    position: absolute;
+    left: -28px;
+    top: 108px;
+    width: 80px;
+  }
 }
 .geneCanvas {
-    cursor: url('~@/assets/images/cur.png'), auto;
+  cursor: url("~@/assets/images/cur.png"), auto;
 }
 .gene-info {
-    min-width: 100px;
-    // display: none;
-    background-color: rgba(11, 3, 4, 0.5);
-    padding: 10px;
-    text-align: left;
-    color: #ffffff;
-    // position: absolute;
-    font-family: arial;
-    font-size: 16px;
-    z-index: 4000;
-    border-radius: 5px;
+  min-width: 100px;
+  // display: none;
+  background-color: rgba(11, 3, 4, 0.5);
+  padding: 10px;
+  text-align: left;
+  color: #ffffff;
+  // position: absolute;
+  font-family: arial;
+  font-size: 16px;
+  z-index: 4000;
+  border-radius: 5px;
 }
 </style>
--- src/views/variation/LDViewer/components/ld-viewer.vue
+++ src/views/variation/LDViewer/components/ld-viewer.vue
@@ -10,12 +10,12 @@
               <div class="reference-item select-item">
                 <span>Reference</span>
                 <el-form-item>
-                  <el-select filterable="" v-model="formData.reference" placeholder="" @focus="focusSelect('reference')">
+                  <el-select filterable="" v-model="formData.accession" placeholder="" >
                     <el-option
-                      v-for="(item,i) in options.reference"
+                      v-for="(item,i) in options.accession"
                       :key="i"
                       :label="item"
-                      value="1"
+                      :value="item"
                       ></el-option>
                   </el-select>
                 </el-form-item>
@@ -23,12 +23,12 @@
               <div class="version-item select-item">
                 <span>Version</span>
                 <el-form-item>
-                  <el-select filterable="" v-model="formData.version" placeholder="" >
+                  <el-select filterable v-model="formData.version" placeholder="" @focus="checkVersionIsNull" >
                     <el-option
                       v-for="(item,i) in options.version"
                       :key="i"
                       :label="item"
-                      value="1"
+                      :value="item"
                       ></el-option>
                   </el-select>
                 </el-form-item>
@@ -36,16 +36,26 @@
               <div class="population-item select-item">
                 <span>Population</span>
                 <el-form-item>
-                  <el-select v-model="formData.population" placeholder="">
-                    <el-option label="population" value="population"></el-option>
+                  <el-select filterable v-model="formData.alias" placeholder="">
+                    <el-option
+                      v-for="(item,i) in options.alias"
+                      :key="i"
+                      :label="item"
+                      :value="item"
+                      ></el-option>
                   </el-select>
                 </el-form-item>
               </div>
               <div class="analysis-item select-item">
                 <span>Analysis</span>
                 <el-form-item>
-                  <el-select filterable v-model="formData.analysis" placeholder="">
-                    <el-option label="analysis" value="analysis"></el-option>
+                  <el-select filterable v-model="formData.description" placeholder="" @focus="checkAnalysis()">
+                    <el-option
+                      v-for="(item,i) in options.description"
+                      :key="i"
+                      :label="item"
+                      :value="item"
+                      ></el-option>
                 </el-select>
                 </el-form-item>
               </div>
@@ -60,8 +70,13 @@
                 <div class="chr">
                   <span>chr</span>
                   <el-form-item>
-                  <el-select filterable="" v-model="formData.chr" placeholder="">
-                    <el-option label="chr" value="chr"></el-option>
+                  <el-select filterable v-model="formData.chorm" placeholder="" @focus="checkChr()">
+                    <el-option
+                      v-for="(item,i) in options.chorm"
+                      :key="i"
+                      :label="item"
+                      :value="item"
+                      ></el-option>
                   </el-select>
                 </el-form-item>
                 </div>
@@ -90,59 +105,88 @@
   </ZeamapCard>
 </template>
 <script>
-import { dropDownReference, dropDownVersion} from '@/api/gemo-viewer/geno-viewer'
+import { dropDownReference, dropDownVersion, dropDownPopulation,dropDownAnalysis,dropDownChr} from '@/api/gemo-viewer/geno-viewer'
 import SvgIcon from '@/components/CommonComponents/SvgIcon.vue'
 export default {
 components: { SvgIcon },
   data() {
     return {
+      isFirst: true,
       region: '1',
       viewerTitle: 'Geno viewer',
       formData: {
-        reference: '',
-        version: '',
-        population: '',
-        analysis: '',
+        accession: "",
+        version: "",
+        alias: "",
+        description: "",
         start: '',
         end: '',
-        chr: '',
+        chorm: '',
       },
       checkBox: [],
       options: {
-        reference: [],
-        version: []
+        accession: [],
+        version: [],
+        alias: [],
+        description: []
       }
+    }
+  },
+  watch: {
+    'formData.accession'(newValue, oldValue) {
+      // 处理 accession 属性变化的逻辑
+      console.log('accession 变化了:', newValue);
+      this.dropDownVersion({accession: newValue})
     }
   },
   created() {
     this.dropDownReference()
-    this.dropDownVersion()
+    this.dropDownPopulation()
   },
   methods: {
+    // version是否为空
+    checkVersionIsNull() {
+      if(this.formData.accession==='') {
+        this.$message.error('请先选择Reference')
+      }
+    },
     // 获取下拉框信息
     async dropDownReference() {
       const { data }= await dropDownReference()
-      let arr = Object.values(data)
-      arr = arr.slice(1,51)
-      this.options.reference = arr
+      this.options.accession = data
     },
-    async dropDownVersion() {
-      const { data }= await dropDownVersion()
-      let arr = Object.values(data)
-      arr = arr.slice(1,51)
-      this.options.version = arr
+    async dropDownVersion(params) {
+      const { data }= await dropDownVersion(params)
+      this.options.version = data
     },
-    // 疯狂道歉
-    focusSelect(name) {
-      if(this.options[name].length === 0) {
-        this.$notify({
-          title: '已成功请求',
-          message: '正在拉取数据，请稍等',
-          type: 'success'
-        })
+    async dropDownPopulation() {
+      const { data }= await dropDownPopulation()
+      this.options.alias = data
+    },
+    async dropDownAnalysis() {
+      const { data }= await dropDownAnalysis(this.formData)
+      this.options.description = data
+    },
+    async dropDownChr() {
+      const { data }= await dropDownChr(this.formData)
+      this.options.chorm = data
+    },
+    checkAnalysis() {
+      const { accession, version, alias } = this.formData;
+      if(accession !== '' && version !== '' && alias !== '') {
+        this.dropDownAnalysis()
+      } else {
+        this.$message.error('请先选择前边三项')
       }
     },
-
+    checkChr() {
+      const { accession, version, alias,description } = this.formData;
+      if(accession !== '' && version !== '' && alias !== '' && description!=="") {
+        this.dropDownChr()
+      } else {
+        this.$message.error('请先选择前边四项')
+      }
+    },
     // 重置所有条件
     reset() {
       this.region= '1',
